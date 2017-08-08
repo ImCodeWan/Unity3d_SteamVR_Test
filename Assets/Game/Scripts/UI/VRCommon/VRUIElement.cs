646f8caeb9c2eeb0c9d38f7def3e16219aa8cb7b
shufengw <imgamer@163.com>
2017-08-08 11:25:24 +0800

    在VR中使用射线选择ui的支持

---
+++ Assets/Game/Scripts/UI/VRCommon/VRUIElement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class VRUIElement : MonoBehaviour 
+{
+    public UnityEvent m_onButtonClick;
+
+	// Use this for initialization
+	void Awake() 
+    {
+        Button button = gameObject.GetComponent<Button>();
+        if(button != null)
+        {
+            button.onClick.AddListener(OnButtonClick);
+        }
+	}
+
+    void Start()
+    {
+        if (m_onButtonClick == null)
+            m_onButtonClick = new UnityEvent();
+    }
+
+    private void OnRayHoverBegin(SelectRaycast p_selectRaycast)
+    {
+        VRInputModule.Instance.HoverBegin(gameObject);
+    }
+
+    private void OnRayHoverEnd(SelectRaycast p_selectRaycast)
+    {
+        VRInputModule.Instance.HoverEnd(gameObject);
+    }
+
+    private void OnRayHoverUpdate(SelectRaycast p_selectRaycast)
+    {
+        //Debug.Log("VRUIElement::OnRayHoverUpdate:" + p_selectRaycast.name);
+        if(p_selectRaycast.GetStandardButtonDown())
+        {
+            VRInputModule.Instance.Submit(this.gameObject);
+        }
+    }
+
+    private void OnButtonClick()
+    {
+        m_onButtonClick.Invoke();
+    }
+}
